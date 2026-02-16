bfa1a21d1c2e0943438cc63288d6f9b6f6e92c87
Alessandro Ducci <duccialessandroofficial@gmail.com>
2026-02-16 17:54:21 +0400

    Finished db connection successfully & migrations. Next: UI + API endpoints.

---
+++ prisma/migrations/20260216132418_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "availability_ranges" (
+    "id" SERIAL NOT NULL,
+    "dayOfWeek" INTEGER NOT NULL,
+    "startTime" TEXT NOT NULL,
+    "endTime" TEXT NOT NULL,
+
+    CONSTRAINT "availability_ranges_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "booked_appointments" (
+    "id" SERIAL NOT NULL,
+    "startDatetimeUtc" TIMESTAMP(3) NOT NULL,
+    "endDatetimeUtc" TIMESTAMP(3) NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "confirmationCode" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "booked_appointments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "booked_appointments_confirmationCode_key" ON "booked_appointments"("confirmationCode");
